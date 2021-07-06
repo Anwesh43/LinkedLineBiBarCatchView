e0b1d2e71205d7f1aaf4a80ba13ffb90ac1e096a
Anwesh43 <anweshthecool0@gmail.com>
2021-07-06 07:44:56 +0530

    adding Canvas extension methods

--- bibarcatchview/src/main/java/com/example/bibarcatchview/BiBarCatchView.kt
+++ bibarcatchview/src/main/java/com/example/bibarcatchview/BiBarCatchView.kt
@@ -31,3 +31,36 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawBiBarCatch(scale : Float, w : Float, h : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val size : Float = Math.min(w, h) / sizeFactor
+    val barW : Float = Math.min(w, h) / barWFactor
+    val barH : Float = Math.min(w, h) / barHFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val uSize : Float = size * sc1
+    save()
+    translate(w / 2 + (w / 2 + barW) * sc4, h / 2)
+    rotate(180f * sc4)
+    drawLine(0f, -uSize / 2, 0f, uSize / 2, paint)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f - 2 * j)
+        translate(w / 2 * (1f - sc2), (uSize * 0.5f - barH) * sc3)
+        drawRect(RectF(0f, -barH / 2, barW, barH / 2), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBBCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBiBarCatch(scale, w, h, paint)
+}
