3dc3df470fedc9829dccb537ba13b06d35bdd1bf
Anwesh43 <anweshthecool0@gmail.com>
2021-07-06 07:57:00 +0530

    adding BiBarCatchView class

--- bibarcatchview/src/main/java/com/example/bibarcatchview/BiBarCatchView.kt
+++ bibarcatchview/src/main/java/com/example/bibarcatchview/BiBarCatchView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawBBCNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBiBarCatch(scale, w, h, paint)
 }
+
+class BiBarCatchView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
