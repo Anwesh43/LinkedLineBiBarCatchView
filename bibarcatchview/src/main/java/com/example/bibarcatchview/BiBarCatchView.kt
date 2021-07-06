1a26a38f66068b0066c8a0109d6b17eb2ceaede4
Anwesh43 <anweshthecool0@gmail.com>
2021-07-06 07:33:16 +0530

    adding constants and libraries

--- bibarcatchview/src/main/java/com/example/bibarcatchview/BiBarCatchView.kt
+++ bibarcatchview/src/main/java/com/example/bibarcatchview/BiBarCatchView.kt
@@ -1,2 +1,29 @@
 package com.example.bibarcatchview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 3.6f
+val barHFactor : Float = 11.2f
+val barWFactor : Float = 6.2f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+
